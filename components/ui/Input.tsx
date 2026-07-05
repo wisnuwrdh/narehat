31c1c31f1642d31e7a0a5c95cbe392d23d077666
OukaXy2 <mywisnu238@gmail.com>
2026-07-05 13:21:14 +0700

    fix: eslint errors - empty interface type + let→const

--- components/ui/Input.tsx
+++ components/ui/Input.tsx
@@ -1,7 +1,7 @@
 import { cn } from "@/lib/utils";
 import type { InputHTMLAttributes } from "react";
 
-export interface InputProps extends InputHTMLAttributes<HTMLInputElement> {}
+export type InputProps = InputHTMLAttributes<HTMLInputElement>;
 
 export function Input({ className, ...props }: InputProps) {
   return (
