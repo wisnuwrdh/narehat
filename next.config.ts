d60dedb381d99ad373d622e9c643e89388b56b1d
OukaXy2 <mywisnu238@gmail.com>
2026-07-05 14:19:09 +0700

    fix: reset postcss config, update next config, remove tsbuildinfo

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,6 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {};
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+};
 
 export default nextConfig;
